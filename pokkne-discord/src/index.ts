04d0e9103b423fbfa02163da751a96643e4499fb
Perttu Autti <65128229+Pertsaa@users.noreply.github.com>
2023-02-27 15:23:16 +0200

    feat: guild messages

--- pokkne-discord/src/index.ts
+++ pokkne-discord/src/index.ts
@@ -1,17 +1,39 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { ChatterServiceRouter } from "service-chatter";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const trpc = createTRPCProxyClient<ChatterServiceRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/trpc",
+      url: "http://localhost:50051/trpc",
     }),
   ],
 });
 
 async function main() {
-  const res = await trpc.chat.query({ message: "msg" });
-  console.log(res);
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+  });
+
+  client.once(Events.ClientReady, (c) => {
+    console.log(`Logged in as ${c.user.tag}`);
+  });
+
+  client.on(Events.MessageCreate, async (m) => {
+    if (client.user && m.author.id === client.user.id) return;
+    console.log(`Received message: ${m.content}`);
+    const res = await trpc.chat.query({ message: m.content });
+    m.reply(res);
+  });
+
+  client.login(process.env.TOKEN);
 }
 
 main();
