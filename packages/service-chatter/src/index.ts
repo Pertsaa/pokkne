0f0ec673b9e2404ee121a28f43eb77688b6e28b1
Perttu Autti <65128229+Pertsaa@users.noreply.github.com>
2023-02-27 15:23:02 +0200

    feat: trpc express adapter

--- packages/service-chatter/src/index.ts
+++ packages/service-chatter/src/index.ts
@@ -1,7 +1,22 @@
-import { initTRPC } from "@trpc/server";
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
 import { z } from "zod";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
 
-const t = initTRPC.create();
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  return {
+    req,
+    res,
+  };
+};
+
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
+
 const router = t.router;
 const publicProcedure = t.procedure;
 
@@ -29,3 +44,26 @@ const appRouter = router({
 });
 
 export type ChatterServiceRouter = typeof appRouter;
+
+async function main() {
+  const app = express();
+
+  app.use((req, _res, next) => {
+    console.log("⬅️ ", req.method, req.path, req.body ?? req.query);
+    next();
+  });
+
+  app.use(
+    "/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    })
+  );
+
+  app.listen(50051, () => {
+    console.log("listening on port 50051");
+  });
+}
+
+main();
